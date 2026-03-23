d49756b513e26765cc5ff2e326e5b2026474b3da
Nikola <n.vasovic@ilexius.de>
2026-03-23 15:29:11 +0100

    staff imp

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  experimental: {
+    cpus: 1,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
