8671ead04eca27a3de431944d536ec37f74aeb71
Nikola <n.vasovic@ilexius.de>
2026-03-23 14:29:11 +0100

    building fixes

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { PrismaLibSql } from "@prisma/adapter-libsql";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
