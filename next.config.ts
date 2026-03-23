0a78e9ff291349b6d96d2ad0ae7229b1dec97a6b
Nikola <n.vasovic@ilexius.de>
2026-03-23 16:00:26 +0100

    staff imp

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
