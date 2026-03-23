f0d9063d9ac8c7ab6515732ad6beff7551acd15c
Nikola <n.vasovic@ilexius.de>
2026-03-23 15:10:15 +0100

    staff imp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import { SALON_NAME, SALON_PHONE, SALON_EMAIL, SALON_ADDRESS } from "@/lib/constants";
 
@@ -147,6 +148,18 @@ export default function RootLayout({
   return (
     <html lang="sr" className={`${playfair.variable} ${inter.variable}`}>
       <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-4Z6DQC653C"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-4Z6DQC653C');
+          `}
+        </Script>
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
